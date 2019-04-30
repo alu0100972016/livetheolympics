331151e5f1a278532aa4e5e6ef0a4535abc726bc
Ana Caceres <alu0100972016@ull.edu.es>
2019-04-30 11:49:45 +0100

    Añadiendo build y heroku

---
+++ dist/precache-manifest.413342b37bde70af7d40d38c77df9bd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "5c261577bf96c4afd25d",
+    "url": "/js/chunk-vendors.c5cba2c3.js"
+  },
+  {
+    "revision": "31252b4a0beb70369d49",
+    "url": "/js/app.434ecf22.js"
+  },
+  {
+    "revision": "a57091e4fa058c862171",
+    "url": "/js/about.3fe48dad.js"
+  },
+  {
+    "revision": "67ae8f0a57e88262e9bffe2054bd1ec8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "5c261577bf96c4afd25d",
+    "url": "/css/chunk-vendors.9506b1a1.css"
+  }
+];
